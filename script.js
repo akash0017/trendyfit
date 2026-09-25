ed8db6e52a2319915473e3acb301fe02916b42fc
akash0017 <141113993+akash0017@users.noreply.github.com>
2026-09-25 19:08:54 +0530

    Update WhatsApp order message format

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// FINAL JS - Photo wala
+// FINAL JS - With Photo
 function orderOnWhatsApp(btn) {
   let card = btn.closest('.product-card');
   let name = card.querySelector('h3').innerText;
@@ -6,6 +6,6 @@ function orderOnWhatsApp(btn) {
   let img = card.querySelector('img').src;
   let page = window.location.pathname.split("/").pop();
   let phone = "916295627939";
-  let message = `Hi TrendyFit Pandaveswar!%0A%0A*Mujhe ye chahiye:*%0A*Product:* ${name}%0A*Price:* ${price}%0A*Page:* ${page}%0A*Photo:* ${img}%0A%0ASize available hai kya?`;
+  let message = `Hi TrendyFit Pandaveswar!%0A%0A*I want this:*%0A*Product:* ${name}%0A*Price:* ${price}%0A*Page:* ${page}%0A*Photo:* ${img}%0A%0AIs this size available?`;
   window.open(`https://wa.me/${phone}?text=${message}`, "_blank");
 }
